2f7bb742c82af9f964c462e095130c8dac349d59
Osky772 <kowalow.oskar@gmail.com>
2019-03-02 22:40:29 +0100

    Add navigation

--- src/.history/index_20190302222949.js
+++ src/.history/index_20190302222949.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./components/Nav/styles.css";
+import "./styles.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
