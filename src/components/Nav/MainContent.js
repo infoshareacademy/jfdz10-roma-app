ffd3953c98e369e777c9d1e2d5d3007d531b655c
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-04 14:28:13 +0100

    User component created and added to route path in MainContent.js.

--- src/components/Nav/MainContent.js
+++ src/components/Nav/MainContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import styled from "styled-components";
+import User from "../UserPanel/User";
 
 const Content = styled.div`
 	width: 100%;
@@ -12,7 +13,7 @@ const MainContent = () => {
 	return (
 		<Content>
 			<Route path="/dashboard" component={() => <h1>Dashboard</h1>} />
-			<Route path="/user-panel" component={() => <h1>User Panel</h1>} />
+			<Route path="/user-panel" component={ User } />
 			<Route path="/create-pizza" component={() => <h1>Create Pizza</h1>} />
 			<Route path="/make-order" component={() => <h1>Make the order</h1>} />
 			<Route
---
+++ src/components/UserPanel/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './user.css';
+
+class User extends React.Component {
+    render (){
+        return(
+            <div class="user__container">
+                <h1>Hello User!</h1>
+            </div>
+        )
+    }
+}
+
+export default User;
