3abbd89bafe1cd9379dfa7423d2b9126ae13b671
Osky772 <kowalow.oskar@gmail.com>
2019-03-05 17:40:20 +0100

    Add button create your own pizza

--- src/components/CreatePizza/CreatePizza.js
+++ src/components/CreatePizza/CreatePizza.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "./create-pizza-button.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import PreviousOrders from "./PreviousOrders";
 
 class CreatePizza extends Component {
+	state = {
+		isCreatePizza: false
+	};
+
+	handleChangeCreatePizza = e => {
+		this.setState({
+			isCreatePizza: true
+		});
+	};
+
 	render() {
 		return (
 			<Container className="h-100">
 				<Row className="h-100">
-					<Col className="d-flex">1 of 2</Col>
+					<Col
+						lg={6}
+						className="d-flex justify-content-center align-items-center"
+					>
+						<Button
+							size="lg"
+							className="create-pizza-button"
+							onClick={this.handleChangeCreatePizza}
+							variant="link"
+						>
+							Create your own pizza
+						</Button>
+					</Col>
 					<Col className="d-flex justify-content-center align-items-center">
 						<PreviousOrders />
 					</Col>
