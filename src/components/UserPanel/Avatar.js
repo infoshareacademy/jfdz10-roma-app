2928f53b9a05455b79f6095f6721455095743e86
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-03-10 20:33:23 +0100

    Img folder moved to public. Dynamic img data in user section (avatar).

--- src/components/UserPanel/Avatar.js
+++ src/components/UserPanel/Avatar.js
@@ -2,9 +2,17 @@ import React from 'react';
 import AvatarImg from './img/avatar.png'
 
 class Avatar extends React.Component {
+    state = {
+        avatar: "",
+    }
+    componentDidMount(){
+        fetch('user.json')
+            .then(response => response.json())
+            .then(value => this.setState({ avatar: value.avatar }))
+    }
     render (){
         return(
-            <img className="user__avatar" src={AvatarImg} alt="avatar" />
+            <img className="user__avatar" src={this.state.avatar} alt="avatar" />
         )
     }
 }
