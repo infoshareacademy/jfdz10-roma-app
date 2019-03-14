aa5b85b74017966ada71c39de84b143c33d6b2ec
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-14 11:01:20 +0100

    AddPizzeria/Dodaj pizzerię is now a button.

--- src/components/UserPanel/AddPizzeria.js
+++ src/components/UserPanel/AddPizzeria.js
@@ -3,7 +3,7 @@ import React from 'react';
 class AddPizzeria extends React.Component {
     render (){
         return(
-            <h2><span role="img" aria-label="building">🏠</span> Dodaj pizzerię</h2>
+            <div></div>
         )
     }
 }
--- src/components/UserPanel/EditProfile.js
+++ src/components/UserPanel/EditProfile.js
@@ -3,8 +3,13 @@ import React from 'react';
 class EditProfile extends React.Component {
     render (){
         return(
-            <div className="edit__button">
-                <span>Edytuj Profil</span>
+            <div className="buttons__container">
+                <div className="button">
+                    <span>Edytuj Profil</span>
+                </div>
+                <div className="button">
+                    <span>Dodaj pizzerię</span>
+                </div>
             </div>
         )
     }
