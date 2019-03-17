8b54b969d43786ec579a8c8f8d69cef01db4950b
joanna-by <j.bykowska@gmail.com>
2019-03-17 13:04:25 +0100

    addingtwo test names to pizzerias.json

--- src/components/CompleteTheOrder/ChosenIngredients.js
+++ src/components/CompleteTheOrder/ChosenIngredients.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+async function fetchPizzerias() {
+	return await fetch("pizzerias.json").then(res => res.json());
+}
+
 const getFromLocalStorage = item => {
 	return JSON.parse(window.localStorage.getItem(item));
 };
@@ -8,13 +12,22 @@ class ChosenIngredients extends React.Component {
     state = {
         ingredients: window.localStorage.getItem("ingredients")
     ? getFromLocalStorage("ingredients")
-    : []
+    : [],
+		pizzerias: [],
+	};
+    
+    componentDidMount() {
+		fetchPizzerias().then(pizzerias => this.setState({ pizzerias }));
     }
+    
     render() {
         return (
         <div>
             <h2>Wybrane przez ciebie składniki:</h2>
             <ul className="completeTheOrderIngredientsList">{this.state.ingredients.map(ingredient => <li>{ingredient.name}</li>)}</ul>
+            <h2>Pizzerie, które zrobią z nich pizze dla ciebie:</h2>
+            <ul>{this.state.pizzerias.map(pizzeria =>
+                 <li>{pizzeria.ingredients === console.log(this.state.ingredients.name) ? pizzeria.name : []}</li>)}</ul>
         </div>
         )}
 }
