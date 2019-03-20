e8e588ceba033b61d9bad3662e6a308a7032a455
Osky772 <kowalow.oskar@gmail.com>
2019-03-20 10:55:45 +0100

    Improve styles for pizzerias list

--- src/components/CompleteTheOrder/PizzeriasList/PizzeriaList.js
+++ src/components/CompleteTheOrder/PizzeriasList/PizzeriaList.js
@@ -1,60 +1,79 @@
-import React, { Component }from "react";
-import Tab from 'react-bootstrap/Tab'
-import ListGroup from 'react-bootstrap/ListGroup'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Tab from "react-bootstrap/Tab";
+import ListGroup from "react-bootstrap/ListGroup";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import "./styles.css";
 
-class PizzeriaList extends Component {  
-    state = {
-        pizzerias: []
-    }
-    componentDidMount() {
-        fetch('pizzerias.json')
-        .then (resp => resp.json())
-        .then (pizzerias => this.setState({pizzerias}))
-    }
-
-
-    
-    render() {
-
-        return <Tab.Container id="list-group-tabs-example list-group-tabs-pizzerias" defaultActiveKey="#link1">
-        <Row>
-          <Col sm={4}>
-          
-            <ListGroup >
-            {
-                this.state.pizzerias.map(pizzeria => {
-                    return (
-                    <ListGroup.Item className key={pizzeria.id} action href={`#${pizzeria.id}`}>
-                        {pizzeria.name}
-                    </ListGroup.Item>
-                    )
-                })
-            }
-            </ListGroup>
-          </Col>
-          <Col sm={8}>
-            <Tab.Content> {
-              this.state.pizzerias.map(pizzeria => {
-                return (
-                    <Tab.Pane key={pizzeria.id}  eventKey={`#${pizzeria.id}`}>
-                    <h1>{pizzeria.name}</h1>
-                    <p>{pizzeria.contactInfo.address.street}</p>
-                    <p>{pizzeria.contactInfo.address.postcode}</p>
-                    <p>{pizzeria.contactInfo.phone}</p>
-                    <p>{pizzeria.contactInfo.website}</p>
-                    </Tab.Pane>
-) })
-            }
-            </Tab.Content>
-          </Col>
-          
-        </Row>    
-      </Tab.Container>;
-               
-    }
+const styles = {
+	RightPane: {
+		background: "white",
+		border: "1px solid lightgray",
+		borderRadius: "5px",
+		padding: "15px"
+	}
 };
 
-export default PizzeriaList
+class PizzeriaList extends Component {
+	state = {
+		pizzerias: []
+	};
+	componentDidMount() {
+		fetch("pizzerias.json")
+			.then(resp => resp.json())
+			.then(pizzerias => this.setState({ pizzerias }));
+	}
+
+	render() {
+		return (
+			<Container
+				style={{ display: "flex", height: "100%", alignItems: "center" }}
+			>
+				<Tab.Container
+					id="list-group-tabs-example list-group-tabs-pizzerias"
+					defaultActiveKey="#link1"
+				>
+					<Row
+						style={{ display: "flex", justifyContent: "center", width: "100%" }}
+					>
+						<Col sm={4}>
+							<ListGroup>
+								{this.state.pizzerias.map(pizzeria => {
+									return (
+										<ListGroup.Item
+											className
+											key={pizzeria.id}
+											action
+											href={`#${pizzeria.id}`}
+										>
+											{pizzeria.name}
+										</ListGroup.Item>
+									);
+								})}
+							</ListGroup>
+						</Col>
+						<Col sm={4} style={styles.RightPane}>
+							<Tab.Content>
+								{" "}
+								{this.state.pizzerias.map(pizzeria => {
+									return (
+										<Tab.Pane key={pizzeria.id} eventKey={`#${pizzeria.id}`}>
+											<h1>{pizzeria.name}</h1>
+											<p>{pizzeria.contactInfo.address.street}</p>
+											<p>{pizzeria.contactInfo.address.postcode}</p>
+											<p>{pizzeria.contactInfo.phone}</p>
+											<p>{pizzeria.contactInfo.website}</p>
+										</Tab.Pane>
+									);
+								})}
+							</Tab.Content>
+						</Col>
+					</Row>
+				</Tab.Container>
+			</Container>
+		);
+	}
+}
+
+export default PizzeriaList;
