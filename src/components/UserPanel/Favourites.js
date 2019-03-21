fc9edd5c6c318909b2a66940aa3b37a9123b1bc5
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-21 16:43:36 +0100

    Debug: display error in user panel if there is no data in localstorage.

--- src/components/UserPanel/Favourites.js
+++ src/components/UserPanel/Favourites.js
@@ -49,35 +49,37 @@ class Favourites extends Component {
 				<h3 className="list-header"><span role="img" aria-label="pizzeria">🏠 </span>Twoje ulubione pizzerie:</h3><br />
 				<ListWrapper className="list-scrollbar">
 					<div className="list-group">
-						{this.state.pizzerias.map((pizzeria, i) => {
-							return (
-								<div
-									key={pizzeria.id}
-									className="list-group-item list-group-item-action flex-column align-items-start"
-									style={{
-										overflowWrap: "break-word",
-										wordWrap: "break-word"
-									}}
-								>
-									<div className="d-flex w-100 justify-content-between">
-										<h5>
-											{this.state.pizzerias[i].name}
-										</h5>
-										<FaHeart
-											onClick={() => this.selectFavPizzeria(pizzeria)}
-											style={
-												this.favIconMarked(pizzeria)
-													? styles.FavIconEnabled
-													: styles.FavIconDisabled
-											}
-										/>
+						{localStorage.getItem("favPizzeria") !== null ?
+							this.state.pizzerias.map((pizzeria, i) => {
+								return (
+									<div
+										key={pizzeria.id}
+										className="list-group-item list-group-item-action flex-column align-items-start"
+										style={{
+											overflowWrap: "break-word",
+											wordWrap: "break-word"
+										}}
+									>
+										<div className="d-flex w-100 justify-content-between">
+											<h5>
+												{this.state.pizzerias[i].name}
+											</h5>
+											<FaHeart
+												onClick={() => this.selectFavPizzeria(pizzeria)}
+												style={
+													this.favIconMarked(pizzeria)
+														? styles.FavIconEnabled
+														: styles.FavIconDisabled
+												}
+											/>
+										</div>
+										<span>Kontakt: {this.state.pizzerias[i].contactInfo.phone}&nbsp;&nbsp;&nbsp;
+											<a href={"http://" + this.state.pizzerias[i].contactInfo.website}>{this.state.pizzerias[i].contactInfo.website}</a>
+										</span>
 									</div>
-									<span>Kontakt: {this.state.pizzerias[i].contactInfo.phone}&nbsp;&nbsp;&nbsp;
-										<a href={"http://" + this.state.pizzerias[i].contactInfo.website}>{this.state.pizzerias[i].contactInfo.website}</a>
-									</span>
-								</div>
-							);
-						})}
+								);
+							}) : null
+						}
 					</div>
 				</ListWrapper>
 			</ListContainer>
