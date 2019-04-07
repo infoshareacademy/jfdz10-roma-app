7181054cfed49b903bc8a64b8ceb6ff067f31b3b
Osky772 <kowalow.oskar@gmail.com>
2019-04-07 20:51:32 +0200

    Add firebase to app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import App from "./App";
 import "./components/styles.css";
 import * as serviceWorker from "./serviceWorker";
 
+var config = {
+	apiKey: "AIzaSyBqMSwUzjjz2rlSppTH-u666-ipft1IVpI",
+	authDomain: "roma-app-e52ca.firebaseapp.com",
+	databaseURL: "https://roma-app-e52ca.firebaseio.com",
+	projectId: "roma-app-e52ca",
+	storageBucket: "roma-app-e52ca.appspot.com",
+	messagingSenderId: "183090459904"
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
