17136e75093262aaf694642043d240312b204670
Osky772 <kowalow.oskar@gmail.com>
2019-04-26 17:53:33 +0200

    Style create pizza butonn with material ui

--- src/components/Pizzerias/PizzeriasList.js
+++ src/components/Pizzerias/PizzeriasList.js
@@ -4,71 +4,91 @@ import Tab from "react-bootstrap/Tab";
 import ListGroup from "react-bootstrap/ListGroup";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { FaHeart } from "react-icons/fa";
+import Button from "react-bootstrap/Button";
+import { withStyles } from "@material-ui/core/styles";
 import "./styles.css";
 
-const styles = {
+const styles = theme => ({
 	RightPane: {
 		background: "white",
 		border: "1px solid lightgray",
 		borderRadius: "5px",
 		padding: "15px"
 	},
-	FavIconEnabled: {
-		float: "right",
-		color: "#cc1a37"
-	},
-	FavIconDisabled: {
-		float: "right",
-		color: "#919191"
+	favButton: {
+		width: 170,
+		height: 38,
+		margin: "0 auto",
+		padding: ".4rem .5rem",
+		fontSize: "1.1rem",
+		border: "none",
+		color: "white",
+		textDecoration: "none",
+		backgroundColor: "#cc3333",
+		"&:active": {
+			color: "black",
+			backgroundColor: "black"
+		},
+		"&:hover": {
+			color: "white",
+			textDecoration: "none",
+			backgroundColor: "#a5182e"
+		},
+		"&:visited": {
+			textDecoration: "none"
+		},
+		"&:focus": {
+			textDecoration: "none",
+			backgroundColor: "#a5182e",
+			boxShadow: "none"
+		}
 	}
-};
+});
 
-function searchFor (term) {
-    return function(x) {
-        return x.name.toLowerCase().includes(term.toLowerCase());
-    }
+function searchFor(term) {
+	return function(x) {
+		return x.name.toLowerCase().includes(term.toLowerCase());
+	};
 }
 
 class PizzeriasList extends Component {
-	state = {}
-  
-    componentDidMount () {
-      fetch('pizzerias.json')
-        .then(res => res.json())
-        .then(this.getData);
+	state = {};
+
+	componentDidMount() {
+		fetch("pizzerias.json")
+			.then(res => res.json())
+			.then(this.getData);
 	}
-  
-    parseData (data) {
-        return data;
-    }
-  
-    getData = (data) => {
-      this.setState({
-        data: this.parseData(data)
-      });
+
+	parseData(data) {
+		return data;
 	}
-	
-	render () {
+
+	getData = data => {
+		this.setState({
+			data: this.parseData(data)
+		});
+	};
+
+	render() {
 		const { data } = this.state;
-	
-		return data ?
-		  this.renderData(data) :
-		  this.renderLoading()
-	  }
-	  
-	  constructor(props){
-		  super(props)
-			  this.state = {
-				  data: this.data,
-				  term: '',
-			  }
-			  this.searchHandler = this.searchHandler.bind(this);
-		  }
-	  
-	  searchHandler (event) {
-		this.setState({term: event.target.value})
-	  }
+		const { classes } = this.props;
+
+		return data ? this.renderData(data, classes) : this.renderLoading();
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: this.data,
+			term: ""
+		};
+		this.searchHandler = this.searchHandler.bind(this);
+	}
+
+	searchHandler(event) {
+		this.setState({ term: event.target.value });
+	}
 
 	selectFavPizzeria = pizzeria => {
 		if (localStorage.getItem("favPizzeria") !== null) {
@@ -95,104 +115,151 @@ class PizzeriasList extends Component {
 		}
 	};
 
-	renderData (pizzerias) {
-		
-		  return (
-		  <div style={{ display: "flex", flexFlow: "column", alignItems: "center"}}>
-		  	<form onSubmit={e => e.preventDefault()} style={{ display: "flex", width: "100vw", flexFlow: "column", height: "18vh", margin: "4px", borderRadius: "1px",}}>
-                <label style={{fontSize: "1.7rem", textAlign: "center", padding: "3px"}}>WYSZUKAJ PIZZERIĘ:<br />
-				<input className="pizzerias-filter-input" style={{}} type= "text" onChange={this.searchHandler}></input></label>
-				<span style={{background: "#f1cd7c", width: "100%", textAlign: "center", padding: "4px"}}>Zacznij wpisywać nazwę pizzerii</span>
-            </form>
-			
-			<Container
-				style={{display: "flex", height: "80vh", alignItems: "center", padding: "1rem" }}
+	renderData(pizzerias, classes) {
+		return (
+			<div
+				style={{ display: "flex", flexFlow: "column", alignItems: "center" }}
 			>
-				<Tab.Container
-					id="list-group-tabs-example list-group-tabs-pizzerias"
-					defaultActiveKey="#link1"
+				<form
+					onSubmit={e => e.preventDefault()}
+					style={{
+						display: "flex",
+						width: "100vw",
+						flexFlow: "column",
+						height: "18vh",
+						margin: "4px",
+						borderRadius: "1px"
+					}}
 				>
-					<Row
-						style={{ display: "flex", justifyContent: "center", width: "100%" }}
+					<label
+						style={{ fontSize: "1.7rem", textAlign: "center", padding: "3px" }}
 					>
-						<Col sm={3}>
-							<ListGroup>
-								{pizzerias.filter(searchFor(this.state.term)).map(pizzeria => {
-									return (
-										<ListGroup.Item
-											className="pizzerias__list__item"
-											key={pizzeria.id}
-											action href={`#${pizzeria.id}`}
-										>
-											{pizzeria.name}
-											<FaHeart
-												onClick={() => this.selectFavPizzeria(pizzeria)}
-												style={
-													this.favIconMarked(pizzeria)
-														? styles.FavIconEnabled
-														: styles.FavIconDisabled
-												}
-											/>
-										</ListGroup.Item>
-									);
-								})}
-							</ListGroup>
-						</Col>
-						<Col sm={8} style={styles.RightPane}>
-							<Tab.Content>
-								{pizzerias.map(pizzeria => {
-									return (
-										<Tab.Pane key={pizzeria.id} eventKey={`#${pizzeria.id}`} className="pizzeriasList__columns__container">
-                                            <div className="pizzeriasList__column__left">
-                                                <div className="pizzeriasList__data">
-                                                    <h1>{pizzeria.name}</h1>
-                                                    <p>
-                                                        Adres: {pizzeria.contactInfo.address.street},<br />
-                                                        {pizzeria.contactInfo.address.postcode}&nbsp;
-                                                        {pizzeria.contactInfo.address.city}
-                                                    </p>
-                                                    <p>Kontakt: {pizzeria.contactInfo.phone}</p>
-                                                    <a target="_blank" href={"http://" + pizzeria.contactInfo.website}>{pizzeria.contactInfo.website}</a>
-                                                </div>
-                                                <div className="pizzeriasList__map">
-                                                </div>
-                                            </div>
-                                            <div className="pizzeriasList__column__right">
-                                                <h1>Menu:</h1>
-                                                    <li>Margherita</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                                    <li>Neapolitana</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                                    <li>Capriciosa</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                                    <li>Salami</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                                    <li>Wegetariańska</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                                    <li>Sycylijska</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                                    <li>Bella</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                                    <li>Własna</li>
-                                                    <h6>&nbsp;&nbsp;Lista składników</h6>
-                                            </div>
-										</Tab.Pane>
-									);
-								})}
-							</Tab.Content>
-						</Col>
-					</Row>
-				</Tab.Container>
-			</Container>
-		</div>
-		)}
-		
-		
-	  
-		renderLoading () {
-		  return <div>Loading...</div>
-		}
-	  }
-	
+						WYSZUKAJ PIZZERIĘ:
+						<br />
+						<input
+							className="pizzerias-filter-input"
+							style={{}}
+							type="text"
+							onChange={this.searchHandler}
+						/>
+					</label>
+					<span
+						style={{
+							background: "#f1cd7c",
+							width: "100%",
+							textAlign: "center",
+							padding: "4px"
+						}}
+					>
+						Zacznij wpisywać nazwę pizzerii
+					</span>
+				</form>
 
-export default PizzeriasList;
+				<Container
+					style={{
+						display: "flex",
+						height: "80vh",
+						alignItems: "center",
+						padding: "1rem"
+					}}
+				>
+					<Tab.Container
+						id="list-group-tabs-example list-group-tabs-pizzerias"
+						defaultActiveKey="#link1"
+					>
+						<Row
+							style={{
+								display: "flex",
+								justifyContent: "center",
+								width: "100%"
+							}}
+						>
+							<Col sm={3}>
+								<ListGroup>
+									{pizzerias
+										.filter(searchFor(this.state.term))
+										.map(pizzeria => {
+											return (
+												<ListGroup.Item
+													className="pizzerias__list__item"
+													key={pizzeria.id}
+													action
+													href={`#${pizzeria.id}`}
+												>
+													{pizzeria.name}
+												</ListGroup.Item>
+											);
+										})}
+								</ListGroup>
+							</Col>
+							<Col sm={8} className={classes.RightPane}>
+								<Tab.Content>
+									{pizzerias.map(pizzeria => {
+										return (
+											<Tab.Pane
+												key={pizzeria.id}
+												eventKey={`#${pizzeria.id}`}
+												className="pizzeriasList__columns__container"
+											>
+												<div className="pizzeriasList__column__left">
+													<div className="pizzeriasList__data">
+														<h1>{pizzeria.name}</h1>
+														<p>
+															Adres: {pizzeria.contactInfo.address.street},
+															<br />
+															{pizzeria.contactInfo.address.postcode}&nbsp;
+															{pizzeria.contactInfo.address.city}
+														</p>
+														<p>Kontakt: {pizzeria.contactInfo.phone}</p>
+														<a
+															target="_blank"
+															href={"http://" + pizzeria.contactInfo.website}
+														>
+															{pizzeria.contactInfo.website}
+														</a>
+													</div>
+													<div className="pizzeriasList__map" />
+												</div>
+												<div className="pizzeriasList__column__right">
+													<h1>Menu:</h1>
+													<li>Margherita</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<li>Neapolitana</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<li>Capriciosa</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<li>Salami</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<li>Wegetariańska</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<li>Sycylijska</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<li>Bella</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<li>Własna</li>
+													<h6>&nbsp;&nbsp;Lista składników</h6>
+													<Button
+														onClick={() => this.selectFavPizzeria(pizzeria)}
+														className={classes.favButton}
+													>
+														Dodaj do ulubionych
+													</Button>
+												</div>
+											</Tab.Pane>
+										);
+									})}
+								</Tab.Content>
+							</Col>
+						</Row>
+					</Tab.Container>
+				</Container>
+			</div>
+		);
+	}
+
+	renderLoading() {
+		return <div>Loading...</div>;
+	}
+}
+
+export default withStyles(styles)(PizzeriasList);
