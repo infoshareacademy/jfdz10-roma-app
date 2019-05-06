3bae4984e3cd7795336369370f6f42b809ad1ee8
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-05-06 19:57:59 +0200

    Display name of user.

--- src/components/UserPanel/UserPanel.js
+++ src/components/UserPanel/UserPanel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from 'firebase'
 import Avatar from './Avatar';
 import UserData from './UserData';
 import Favourites from './Favourites';
@@ -7,13 +8,47 @@ import Auth from '../Auth/Auth'
 import './user.css';
 
 class User extends React.Component {
-    
+    state = {
+        user: null
+    }
+
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user =>
+            this.setState({
+                authUser: user,
+                authUserId: user.uid,
+                authUserEmail: user.email,
+                authUserRegistered: user.metadata.creationTime,
+                authIsChecked: true,
+            }),
+        )
+        const databaseRef = firebase.database().ref('users')
+        databaseRef.once('value')
+            .then(snapshot => {
+                const snapshotVal = snapshot.val() || {};
+                const findUser = Object.keys(snapshotVal)
+                    .map(key => ({
+                        id: key,
+                        ...snapshotVal[key]
+                    }))
+                    .filter(user => {
+                        return user.id === this.state.authUserId
+                    })
+                const user = findUser[0]
+                this.setState({ 
+                    user, 
+                    userFirstName: user.name.split(' ')[0] 
+                })
+            })
+    }
+
     render (){
         return(
             <div className="user__container">
                 <Auth>
                     <div className="user__container__left">
-                        <h1 className="sayHello"><span role="img" aria-label="pizza">🍕</span> Witaj w Pizzerium!</h1>
+                        {console.log(this.state.user)}
+                        <h1 className="sayHello"><span role="img" aria-label="pizza">🍕</span> Witaj {this.state.user ? this.state.userFirstName : 'w Pizzerium'}</h1>
                         <Avatar />
                         <UserData />
                     </div>
