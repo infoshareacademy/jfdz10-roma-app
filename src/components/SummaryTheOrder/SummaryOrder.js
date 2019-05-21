2440584faadeb443b8f1641e71079e7b7d70c318
Osky772 <kowalow.oskar@gmail.com>
2019-05-21 19:41:30 +0200

    Display selected pizzeria

--- src/components/SummaryTheOrder/SummaryOrder.js
+++ src/components/SummaryTheOrder/SummaryOrder.js
@@ -1,10 +1,63 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Button from "react-bootstrap/Button";
+import { Divider } from "@material-ui/core";
+
+const styles = theme => ({
+	wrapper: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		width: "100%",
+		height: "100%"
+	},
+	header: {
+		width: "100%"
+	},
+	pizzeriaWrapper: {
+		maxWidth: 400,
+		width: "100%"
+	},
+	pizzeria: {
+		padding: 15,
+		textAlign: "center"
+	}
+});
+
+const getFromLocalStorage = item => {
+	return JSON.parse(window.localStorage.getItem(item));
+};
 
 class SummaryOrder extends Component {
-	state = {};
+	state = {
+		pizzeria: getFromLocalStorage("selectedPizzeria"),
+		ingredients: getFromLocalStorage("ingredients")
+	};
+
 	render() {
-		return <h1>Summary order</h1>;
+		const { pizzeria, ingredients } = this.state;
+		const { classes } = this.props;
+		console.log(this.state);
+		return (
+			<div className={classes.wrapper}>
+				<h1 className={classes.header}>Podsumowanie zamówienia</h1>
+				<div className={classes.pizzeriaWrapper}>
+					<h2>Wybrana pizzeria: </h2>
+					<Paper className={classes.pizzeria}>
+						<h2>{pizzeria.name}</h2>
+						<p>{pizzeria.contactInfo.address.street}</p>
+						<p>
+							{pizzeria.contactInfo.address.postcode}{" "}
+							{pizzeria.contactInfo.address.city}
+						</p>
+						<p>{pizzeria.contactInfo.phone}</p>
+						<p>{pizzeria.contactInfo.website}</p>
+					</Paper>
+				</div>
+			</div>
+		);
 	}
 }
 
-export default SummaryOrder;
+export default withStyles(styles)(SummaryOrder);
