61a718fed985b5cc6ac1f1e04d3ac4d4ecf16908
joanna-by <j.bykowska@gmail.com>
2019-05-23 19:12:21 +0200

    Add state in a pie chart component

--- src/components/Dashboard/Chart2.js
+++ src/components/Dashboard/Chart2.js
@@ -4,17 +4,24 @@ const PieChart = require("react-chartjs").Pie;
 
 
 class Chart2 extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            allSoldPizzas: 20,
+            customsPizzas: 15
+        }
+    }
     
     render() {
         const chart2Data = [
             {
-                value: 300,
+                value: this.state.allSoldPizzas,
                 color:"#F7464A",
                 highlight: "#FF5A5E",
                 label: "Wszystkie pizze"
             },
             {
-                value: 100,
+                value: this.state.customsPizzas,
                 color: "#FDB45C",
                 highlight: "#FFC870",
                 label: "Pizze użytkowników"
